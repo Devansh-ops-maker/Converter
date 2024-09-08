a42f003d0de14cc90d3b15e6c8707ef82b646c66
Devansh-ops-maker <ds109@snu.edu.in>
2024-09-08 16:42:23 +0530

    Add files via upload

---
+++ converter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+int n;
+
+printf("Enter a number in decimal format");
+
+scanf("%d",&n);
+
+printf("The given number in octal format is %o: \n",n);
+
+printf("The given number in hexadecimal format is: %x \n",n);
+
+}
